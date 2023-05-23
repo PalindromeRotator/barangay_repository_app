0bcc71acbd82c6a91c2a8a5691851e1c60ca830e
Kenneth Galvez <57065417+PalindromeRotator@users.noreply.github.com>
2023-05-23 10:04:06 +0800

    Navigation bars

---
+++ lib/widgets/containers/pages/tabs/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Home Page')),
+        body: Container(
+          child: Center(child: Text('Home Page')),
+        ));
+  }
+}
--- lib/widgets/containers/pages/tabs/main_tab.dart
+++ lib/widgets/containers/pages/tabs/main_tab.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+import 'package:barangay_repository_app/widgets/containers/pages/tabs/home.dart';
+import 'package:barangay_repository_app/widgets/containers/pages/tabs/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -14,40 +16,49 @@ class MainTab extends StatefulWidget {
 
 class _MainTabState extends State<MainTab> {
   PageController _pageController = new PageController();
+  int _selectedIndex = 0;
+  int maxCount = 2;
+
+  /// widget list
+  final List<Widget> bottomBarPages = [const HomePage(), const ProfilePage()];
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      print(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-      bottomNavigationBar: AnimatedNotchBottomBar(
-        pageController: _pageController,
-        bottomBarItems: [
-          const BottomBarItem(
-            inActiveItem: Icon(
-              Icons.home_filled,
-              color: Colors.blueGrey,
+        body: bottomBarPages.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.red,
             ),
-            activeItem: Icon(
-              Icons.home_filled,
-              color: Colors.blueAccent,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Me',
+              backgroundColor: Colors.green,
             ),
-            itemLabel: 'Page 1',
-          ),
-          const BottomBarItem(
-            inActiveItem: Icon(
-              Icons.star,
-              color: Colors.blueGrey,
-            ),
-            activeItem: Icon(
-              Icons.star,
-              color: Colors.blueAccent,
-            ),
-            itemLabel: 'Page 2',
-          ),
-        ],
-        onTap: (int value) {
-          print(value);
-        },
-      ),
-    );
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ));
   }
 }
